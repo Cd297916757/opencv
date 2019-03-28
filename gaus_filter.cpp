f1ea0770beae571422ae44331a2ac1667c25afca
lin <389038968@qq.com>
2019-03-28 20:50:31 +0800

    add gaus-high-pass

--- gaus_filter.cpp
+++ gaus_filter.cpp
@@ -7,26 +7,27 @@
 using namespace std;
 using namespace cv;
 
-#define size 5//卷积核大小
-#define sigma 1//卷积核的标准差大小
+#define size 3//卷积核大小
+#define sigma 1.5//sigma越大，平滑效果越明显
 
 double gaus[size][size];
 
-void GetGaussianKernel()
+void GetgaussianKernel()
 {
 	const double PI = 4.0*atan(1.0);//π
 	int center = size / 2;
 	double sum = 0;
+	//高斯低通滤波
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
 		{
-			//(ceter,ceter)为中心点坐标
-			gaus[i][j] = (1 / (2 * PI*sigma*sigma))*exp(-((i - center)*(i - center) + (j - center)*(j - center)) / (2 * sigma*sigma));
+			//忽略公式中的(1 / (2 * PI*sigma*sigma))*，不影响结果
+			gaus[i][j] = exp(-((i - center)*(i - center) + (j - center)*(j - center)) / (2 * sigma*sigma));
 			sum += gaus[i][j];
 		}
 	}
-	cout << "卷积核矩阵如下：" << endl;
+	cout << "低通滤波矩阵：" << endl << endl;
 	for (int i = 0; i < size; i++)
 	{
 		for (int j = 0; j < size; j++)
@@ -40,9 +41,9 @@ void GetGaussianKernel()
 
 int main()
 {
-	Mat srcImage = imread("marilyn.bmp");
+	Mat src_image = imread("einstein.bmp");
 	//判断图像是否加载成功
-	if (srcImage.data)
+	if (src_image.data)
 		cout << "图像加载成功!" << endl << endl;
 	else
 	{
@@ -50,16 +51,20 @@ int main()
 		system("pause");
 		return -1;
 	}
-	namedWindow("srcImage", WINDOW_AUTOSIZE);
-	imshow("srcImage", srcImage);
+	namedWindow("src_image", WINDOW_AUTOSIZE);
+	imshow("src_image", src_image);
 
-	GetGaussianKernel();//生成高斯卷积核矩阵
-	Mat kern = Mat(size, size, CV_64F, gaus);
+	GetgaussianKernel();//生成高斯卷积核矩阵
+	Mat kern_low = Mat(size, size, CV_64F, gaus);
+	Mat dst_image_low,dst_image_high;
+	filter2D(src_image, dst_image_low, src_image.depth(), kern_low);
+	//addWeighted方法进行图像相减
+	addWeighted(src_image, 1, dst_image_low, -1, 0, dst_image_high);
 
-	Mat dstImage;
-	filter2D(srcImage, dstImage, srcImage.depth(), kern);
-	namedWindow("dstImage", WINDOW_AUTOSIZE);
-	imshow("dstImage", dstImage);
+	namedWindow("low_pass", WINDOW_AUTOSIZE);
+	namedWindow("high_pass", WINDOW_AUTOSIZE);
+	imshow("low_pass", dst_image_low);
+	imshow("high_pass", dst_image_high);
 
 	waitKey(0);
 	return 0;
