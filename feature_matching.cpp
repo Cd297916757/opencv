278aaa2bb6e3c37aeece770fbd6febcc3d38b13e
lin <389038968@qq.com>
2019-05-04 21:28:29 +0800

    finish BRIEF,need to draw and match keypoints

--- feature_matching.cpp
+++ feature_matching.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <random>
 
 using namespace std;
 using namespace cv;
@@ -9,9 +10,27 @@ using namespace cv;
 #define k_size 3//卷积核大小
 #define sigma 1//sigma越大，平滑效果越明显
 #define threshold 15//FAST的阈值
+//=====BRIEF描述子部分=====
+#define s 10//半径
+#define Time 10//随机选取的点对
 
 const double PI = 4.0*atan(1.0);//π
 double gaus[k_size][k_size];
+
+class node {
+public:
+	int x;
+	int y;
+	node(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	node()
+	{
+		this->x = 0;
+		this->y = 0;
+	}
+};
 
 void GetgaussianKernel()
 {
@@ -54,7 +73,7 @@ int compare(uchar a, uchar b)
 }
 
 //Features from  Accelerated Segment Test
-void FAST(Mat &src,Mat &area)
+void FAST(Mat src,Mat &area)
 {
 	int row = src.rows;
 	int col = src.cols;
@@ -130,6 +149,40 @@ void FAST(Mat &src,Mat &area)
 	}
 }
 
+double GaussRand()
+{
+	random_device rd;
+	mt19937 gen(rd());
+	//normal(0,1)中0为均值，1为方差
+	normal_distribution<double> normal(0, s*s/25);
+	return round(normal(gen));
+}
+
+//p和q都符合(0,s*s/25)的高斯分布 特征点周围sxs的区域
+void BRIEF(Mat src,int desc[Time])
+{
+	//生成符合高斯分布的随机坐标
+	node p[Time], q[Time];
+	for (int i = 0; i < Time; i++)
+	{
+		p[i].x = GaussRand();
+		p[i].y = GaussRand();	
+	}
+	for (int i = 0; i < Time; i++)
+	{
+		q[i].x = GaussRand();
+		q[i].y = GaussRand();
+	}
+
+	for (int i = 0; i < Time; i++)
+	{
+		if (src.at<uchar>(p[i].x, p[i].y) > src.at<uchar>(q[i].x, q[i].y))
+			desc[i] = 1;
+		else
+			desc[i] = 0;
+	}
+}
+
 int main()
 {
 	Mat src1 = imread("1.jpg");
@@ -143,29 +196,86 @@ int main()
 		system("pause");
 		return -1;
 	}
-	Mat src1_gray,src1_low;
 
+	Mat src1_gray, src1_low;
+	Mat src2_gray, src2_low;
 	//灰度处理，高斯滤波
 	cvtColor(src1, src1_gray, COLOR_BGR2GRAY);
+	cvtColor(src2, src2_gray, COLOR_BGR2GRAY);
 	src1_low = GaussianFilter(src1_gray);
+	src2_low = GaussianFilter(src2_gray);
 
 	Mat area1 = Mat::ones(src1_low.rows, src1_low.cols, CV_8UC1);
+	Mat area2 = Mat::ones(src2_low.rows, src2_low.cols, CV_8UC1);
 	area1 += Scalar::all(254);
+	area2 += Scalar::all(254);
 	FAST(src1_low, area1);
+	FAST(src2_low, area2);
 
-	Mat dst1 = src1.clone();
-	for (int i = 0; i < dst1.rows; i++)
+	//后面可以考虑调用库函数来画点
+	Mat src1_fast = src1.clone();
+	Mat src2_fast = src2.clone();
+	int num1 = 0, num2 = 0;
+	for (int i = 0; i < src1_fast.rows; i++)
 	{
-		for (int j = 0; j < dst1.cols; j++)
+		for (int j = 0; j < src1_fast.cols; j++)
 		{
 			if (area1.at<uchar>(i, j) == 255)
 			{
-				dst1.at<Vec3b>(i, j) = 0;
-				dst1.at<Vec3b>(i, j)[2] = 255;
+				src1_fast.at<Vec3b>(i, j) = 0;
+				src1_fast.at<Vec3b>(i, j)[2] = 255;
+				num1++;
 			}
 		}
 	}
-	imshow("dst1", dst1);
+	for (int i = 0; i < src2_fast.rows; i++)
+	{
+		for (int j = 0; j < src2_fast.cols; j++)
+		{
+			if (area2.at<uchar>(i, j) == 255)
+			{
+				src2_fast.at<Vec3b>(i, j) = 0;
+				src2_fast.at<Vec3b>(i, j)[2] = 255;
+				num2++;
+			}
+		}
+	}
+	imshow("src1特征点", src1_fast);
+	imshow("src2特征点", src2_fast);
+
+	//存储描述子的二进制码，Descriptor
+	//int desc1[Time];
+	//int desc2[Time];
+	int **desc1 = new int *[num1];
+	int **desc2 = new int *[num2];
+	for (int i = 0; i < num1; i++)
+	{
+		desc1[i] = new int[Time];
+		BRIEF(src1_low, desc1[i]);
+	}
+	for (int i = 0; i < num2; i++)
+	{
+		desc2[i] = new int[Time];
+		BRIEF(src2_low, desc2[i]);
+	}
+
+	for (int i = 0; i < num1; i++)
+	{
+		for (int j = 0; j < num2; j++)
+		{
+			for (int k = 0; k < Time; k++)
+			{
+				if (desc1[i][k] != desc2[j][k])
+					break;
+				//匹配后如何存储结果，方便绘画点对
+				if ((desc1[i][k] == desc2[j][k]) && (k = Time - 1))//match
+					;
+			}
+		}
+	}
+	//Mat dst1 = src1.clone();
+	//Mat dst2 = src2.clone();
+	//imshow("dst1", dst1);
 
 	waitKey(0);
 	return 0;
